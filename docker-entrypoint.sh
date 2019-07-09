be8089c1005aefa51863c105d71560bdff801d00
Andrey Tyshev <slowprog.job@gmail.com>
2019-07-09 12:03:24 +0300

    Update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ echo "Starting Syncthing"
 
 chown $PUID:$PGID -R /sync
 # see https://docs.syncthing.net/users/faq.html#inotify-limits
-echo "fs.inotify.max_user_watches=204800" | tee -a /etc/sysctl.conf
+echo "fs.inotify.max_user_watches=1000000" | tee -a /etc/sysctl.conf
