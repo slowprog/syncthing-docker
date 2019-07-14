7707375ac0f409d0769088028eee1a1a835e831d
Andrey Tyshev <slowprog.job@gmail.com>
2019-07-14 15:39:57 +0300

    Update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+
+#!/usr/bin/with-contenv bash
 
 echo "Starting Syncthing"
 
-chown $PUID:$PGID -R /sync
+chown "${PUID}:${PGID}" -R /sync
+
 # see https://docs.syncthing.net/users/faq.html#inotify-limits
 echo "fs.inotify.max_user_watches=1000000" | tee -a /etc/sysctl.conf
+
+UMASK_SET=${UMASK_SET:-022}
+
+umask "$UMASK_SET"
+
+exec \
+	s6-setuidgid abc syncthing \
+	-home=/config -no-browser -no-restart \
+	--gui-address="0.0.0.0:8384"
